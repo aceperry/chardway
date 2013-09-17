d66e04a308cfc5ad59c4770f8984cb7854f417b2
Perry <perrychow@yahoo.com>
2013-09-16 21:42:46 -0700

    new ex7

---
+++ ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int maink(int argc, char *argv[])
+{
+	int bugs = 100;
+	double bug_rate = 1.2;
+
+	printf("You have %d bugs at the imaginary rate of $f.\n", bugs, bug_rate);
+
+	printf(".\n",);
+
+printf(".\n",);
+
+printf(".\n",);
+
+}
