2d31447e91036cd65e82c2ac8b5c7703981c6be6
perry <perry@raptor2>
2013-09-25 21:12:10 -0700

    new file:   .cproject 	new file:   .project 	modified:   e8b 	modified:   e8b.c 	new file:   ex8break 	new file:   ex8break.c

--- e8b.c
+++ e8b.c
@@ -11,13 +11,13 @@ int main(int argc, char const *argv[])
 	long double vldouble;
 	bool vbln;
 
-	printf("Size of int is %d\n", sizeof(vint));
-	printf("Size of long is %d\n", sizeof(vlong));
-	printf("Size of long long is %d\n", sizeof(vllong));
-	printf("Size of float is %d\n", sizeof(vfloat));
-	printf("Size of double is %d\n", sizeof(vdouble));
-	printf("Size of long double is %d\n", sizeof(vldouble));
-	printf("Size of boolean is %d\n", sizeof(vbln));
+	printf("Size of int is %lu\n", sizeof(vint));
+	printf("Size of long is %lu\n", sizeof(vlong));
+	printf("Size of long long is %lu\n", sizeof(vllong));
+	printf("Size of float is %lu\n", sizeof(vfloat));
+	printf("Size of double is %lu\n", sizeof(vdouble));
+	printf("Size of long double is %lu\n", sizeof(vldouble));
+	printf("Size of boolean is %lu\n", sizeof(vbln));
 
 	return 0;
 }
