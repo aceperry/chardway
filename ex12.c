ddfc67065d680debe33c571dfd5d0cc193c16dd9
Perry <perry@raptor2>
2014-03-06 03:07:59 -0800

    Finish ex12

--- ex12.c
+++ ex12.c
@@ -5,18 +5,18 @@ int main(int argc, char *argv[])
     int i = 0;
     
     if(argc == 1) {
-        printf("You only have one argument.  You suck./n");
+        printf("You only have one argument.  You suck.\n");
         }  else if(argc > 1 && argc < 4)  {
             printf("Here's your arguments:\n");
             
-            for(i = 0, i < argc, ++i)  {
+            for(i = 0; i < argc; ++i)  {
                 printf("%s ", argv[i]);
             }
-            print("\n");
+            printf("\n");
         }  else  {
             printf("You have too many arguments.  You suck.\n");
         }
         
-        return 0;
+        return (0);
     }
         
