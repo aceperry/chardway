58b368d073e5a3810287ac6d1f8e6eca9e1639cd
Perry <perry@pixel.com>
2014-03-05 22:34:50 -0800

    refactor else if statements

--- ex12.c
+++ ex12.c
@@ -6,16 +6,14 @@ int main(int argc, char *argv[])
     
     if(argc == 1) {
         printf("You only have one argument.  You suck./n");
-        }
-    	else if(argc > 1 && argc < 4)  {
+        }  else if(argc > 1 && argc < 4)  {
             printf("Here's your arguments:\n");
             
             for(i = 0, i < argc, ++i)  {
                 printf("%s ", argv[i]);
             }
             print("\n");
-        }
-    	else  {
+        }  else  {
             printf("You have too many arguments.  You suck.\n");
         }
         
